6407257fecc5cc9de4959f70585d1d49e97a97b7
naks1 <naks1@LAPTOP-ALF4K35C>
2023-08-10 10:47:23 -0500

    Add project files.

---
+++ PaintballGun.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaintBallGame
+{
+    class PaintballGun
+    {
+        public const int MAGAZINE_SIZE = 16;
+        private int balls = 0;
+        private int ballsLoaded = 0;
+        public int BallsLoaded
+        {
+            get { return ballsLoaded; }
+            set { ballsLoaded = value; } //value is readonly that contains the value being set.
+        }
+
+        //You can make your automatic property read-only by making its setter private
+        // public int BallsLoaded { get; private set; }
+        public bool IsEmpty() { return BallsLoaded == 0; }
+        //public int GetBalls() { return balls; }
+        //public void SetBalls(int numberOfBalls)
+        //{
+        //    if (numberOfBalls > 0) balls = numberOfBalls;
+        //    Reload();
+        //}
+        public int Balls
+        {
+            get { return balls; }
+            set
+            {
+                if (value > 0) balls = value;
+                Reload();
+            }
+              
+        }
+        public void Reload()
+        {
+            if (balls > MAGAZINE_SIZE) ballsLoaded = MAGAZINE_SIZE;
+            else ballsLoaded = balls;
+        }
+        public bool Shoot()
+        {
+            if (ballsLoaded == 0) return false;
+            ballsLoaded--;
+            balls--;
+            return true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace PaintBallGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PaintballGun gun = new PaintballGun();
+            while (true)
+            {
+                Console.WriteLine($"{gun.Balls} balls, {gun.BallsLoaded} loaded");
+                //Console.WriteLine($"{gun.GetBalls()} balls, {gun.GetBallsLoaded()} loaded");
+                if (gun.IsEmpty()) Console.WriteLine("WarningException: You're out of amo");
+                Console.WriteLine("Space to shoot, r to reload, + to add amo, q to quit");
+                char key = Console.ReadKey(true).KeyChar;
+                if (key == ' ') Console.WriteLine($"Shooting returned {gun.Shoot()}");
+                else if (key == 'r') gun.Reload();
+                else if (key == '+') gun.Balls += PaintballGun.MAGAZINE_SIZE;
+                //else if (key == '+') gun.SetBalls(gun.GetBalls() + PaintballGun.MAGAZINE_SIZE);
+                else if (key == 'q') return;
+            }
+        }
+    }
+}
